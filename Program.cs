b8ef8585a832419182664944b31985e4938b1d26
GabrielSantanaNeto <gabrielsantananeto@yahoo.com.br>
2024-09-08 11:19:05 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("NOKIA");
+Smartphone nokia = new Nokia("1", "Modelo1", "NK1", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("DIO");
+
+Console.WriteLine("-----------------");
+
+Console.WriteLine("IPHONE");
+Smartphone iphone= new Iphone("22", "11", "432444", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Maça");
+
+
+
+
+
